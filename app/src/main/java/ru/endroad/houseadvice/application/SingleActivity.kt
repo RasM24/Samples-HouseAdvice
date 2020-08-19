abe826ebfe05ce459db362e28faa1c1c0f2d9007
dvoljatik <o.dvoljatik@cft.ru>
2020-08-19 20:30:49 +0700

    Перенес SingleActivity в пакет application

--- app/src/main/java/ru/endroad/houseadvice/application/SingleActivity.kt
+++ app/src/main/java/ru/endroad/houseadvice/application/SingleActivity.kt
@@ -1,8 +1,9 @@
-package ru.endroad.houseadvice
+package ru.endroad.houseadvice.application
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_single.toolbar
+import ru.endroad.houseadvice.R
 
 class SingleActivity : AppCompatActivity() {
 
