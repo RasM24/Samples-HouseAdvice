36f01b4b4b3d86c5d2f8cf69b07915a5b46705c0
dvoljatik <o.dvoljatik@cft.ru>
2020-08-23 01:21:17 +0700

    Пришлось удалить push-уведомления, где то закрался косяк

--- app/src/main/java/ru/endroad/houseadvice/navigation/di/NavigationModule.kt
+++ app/src/main/java/ru/endroad/houseadvice/navigation/di/NavigationModule.kt
@@ -6,14 +6,12 @@ import ru.endroad.feature.navigation.HubNavigationRouter
 import ru.endroad.feature.onboard.di.OnBoardRouting
 import ru.endroad.feature.question.list.di.QuestionListRouter
 import ru.endroad.houseadvice.R
-import ru.endroad.houseadvice.feature.push.PushRouting
 import ru.endroad.houseadvice.navigation.navigator.ContentNavigator
 import ru.endroad.houseadvice.navigation.navigator.NavigatorHolder
 import ru.endroad.houseadvice.navigation.navigator.RootNavigator
 import ru.endroad.houseadvice.navigation.routers.HubNavigationRouterImpl
 import ru.endroad.houseadvice.navigation.routers.MainNavigation
 import ru.endroad.houseadvice.navigation.routers.OnBoardRoutingImpl
-import ru.endroad.houseadvice.navigation.routers.PushRoutingImpl
 import ru.endroad.houseadvice.navigation.routers.QuestionListRouterImpl
 
 val rootNavigatorQualifier = named("rootNavigatorQualifier")
@@ -24,7 +22,6 @@ val moduleNavigation = module {
 	single<NavigatorHolder>(rootNavigatorQualifier) { RootNavigator(R.id.root) }
 
 	single { MainNavigation(get(rootNavigatorQualifier), get(contentNavigatorQualifier), get()) }
-	single<PushRouting> { PushRoutingImpl(get(contentNavigatorQualifier)) }
 	single<OnBoardRouting> { OnBoardRoutingImpl(get(rootNavigatorQualifier), get(contentNavigatorQualifier)) }
 	single<HubNavigationRouter> { HubNavigationRouterImpl(get(contentNavigatorQualifier)) }
 	single<QuestionListRouter> { QuestionListRouterImpl(get(contentNavigatorQualifier)) }
--- feature-push/src/main/java/ru/endroad/houseadvice/feature/push/PushRouting.kt
+++
@@ -1,8 +0,0 @@
-package ru.endroad.houseadvice.feature.push
-
-import com.google.firebase.messaging.RemoteMessage
-
-interface PushRouting {
-
-	fun openPushNotification(data: RemoteMessage)
-}
